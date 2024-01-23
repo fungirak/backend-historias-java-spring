890922cbddd85d05c5e29875bcb9215063572a11
fungirak <gabilazzarini88@outlook.com>
2024-01-23 16:25:54 -0300

    Intento de CORS resuelto para ruta frontend de producción. Recompilado

--- src/main/java/com/ProyectoFinalArgProg/crud/CrudApplication.java
+++ src/main/java/com/ProyectoFinalArgProg/crud/CrudApplication.java
@@ -1,8 +1,10 @@
 package com.ProyectoFinalArgProg.crud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @CrossOrigin(origins = "https://deploy-next-portafolio-app.vercel.app", 
